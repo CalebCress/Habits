d5242aaed2d30cdfcb6980475d79708e73eb77f7
caleb <33111047+CalebCress@users.noreply.github.com>
2023-02-12 18:20:15 -0500

    Created rest of the pages

---
+++ src/Stage2.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<main>
+    <h1>Stage 2</h1>
+    <p id="instructions">
+        Instructions...
+    </p>
+</main>
+<style>
+    #instructions {
+        padding-left: 12%;
+        padding-right: 12%;
+    }
+</style>
