455b309b36d7b5d42bacc8e418d5d6d983456250
caleb <33111047+CalebCress@users.noreply.github.com>
2023-02-12 07:49:32 -0500

    Cheat Sheet layed out and styling

---
+++ src/lib/CheatSheet.svelte
@@ -0,0 +1,23 @@
+<script>
+    import SheetHabit from "./SheetHabit.svelte";
+</script>
+<main>
+    <div id="tracker">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Daily Habits</th>
+                <th scope="col">+ - =</th>
+            </tr>
+            </thead>
+            <tbody>
+            <SheetHabit row={1}/>
+            <SheetHabit row={2}/>
+            <SheetHabit row={3}/>
+            </tbody>
+        </table>
+    </div>
+</main>
+<style>
+</style>
---
+++ src/lib/SheetHabit.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let row;
+</script>
+<tr>
+    <th scope="row">{row}</th>
+    <td>Mark</td>
+    <td>+</td>
+</tr>
