17d244708e1d40c67d8b671b5413b77e2a9f753f
caleb <33111047+CalebCress@users.noreply.github.com>
2023-02-13 20:43:18 -0500

    Discussion questions page finished with autosave

---
+++ src/DiscussionQuestions.svelte
@@ -0,0 +1,48 @@
+<script>
+// @ts-nocheck
+
+    import {disQuestions} from './questions';
+
+    let questions = JSON.parse(localStorage.getItem('discussion'));
+    if (!questions) {
+        questions = disQuestions;
+        localStorage.setItem('discussion', JSON.stringify(questions));
+    }
+
+    const save = () => {
+        localStorage.setItem('discussion', JSON.stringify(questions));
+    }
+
+</script>
+
+<main>
+    <h1>Discussion Questions</h1>
+    <p id="instructions">
+        Fill out these discussion questions to establish your intentions for creating Atomic Habits. Questions taken from Atomic Habits.
+    </p>
+    {#each questions as question, i}
+        <div class="card question">
+            <label for={i} class="form-label question">{question.question}</label>
+            <div class="input-group">
+                <textarea on:keyup={save} bind:value={question.answer} id={i} class="form-control" aria-label="With textarea"></textarea>
+            </div>
+        </div>
+    {/each}
+</main>
+<style>
+    main {
+        margin: auto;
+        width: 80%;
+    }
+    textarea {
+        resize: none;
+    }
+    .question {
+        text-align: left;
+        margin-bottom: 10px;
+    }
+    #instructions {
+        padding: 10px;
+        text-align: left;
+    }
+</style>
---
+++ src/questions.js
@@ -0,0 +1,42 @@
+export let disQuestions = [
+    {
+        question: `In the past, have you approached your habits with more of a systems-based or goals-based approach? What parts of that approach worked for you, and which parts failed you?`,
+        answer: ``
+    },
+    {
+        question: `Every action is a vote for the type of person you wish to become. Take a moment to imagine the type of person you'd like to become, then brainstorm the small habits that could help you build that new identity.`,
+        answer: ``
+    },
+    {
+        question: `What identities have you adopted that might be holding you back (for example, saying “I'm horrible at math” or “I'm not a morning person”)? How has that self-talk impacted you? How could you reframe that self-talk to be more empowering and positive?`,
+        answer: ``
+    },
+    {
+        question: `Think of a common pattern of behavior in your life and business. Describe the cue, craving, response, and reward for that behavior. (See the charts and examples in Chapter 3 if you need help.)`,
+        answer: ``
+    },
+    {
+        question: `Environment design is one strategy discussed in the book. What steps can you take to design your environment for success? How can you design a world where it's easy to do what's right? What if you live with other people, or are working in a shared space?`,
+        answer: ``
+    },
+    {
+        question: `Have you ever tried to break a bad habit by relying on motivation and willpower? What happened, and what did you learn? Why is habit more reliable than motivation and willpower?`,
+        answer: ``
+    },
+    {
+        question: `Social norms and cultural pressures can play a significant role in shaping our habits. What communities or sub-cultures are you a part of, and how have they impacted your habits?`,
+        answer: ``
+    },
+    {
+        question: `Technology can help us fully automate a habit (like saving for retirement), but it can also create bad habits (like bingewatching videos and television). Have you found technology more helpful or hurtful in your life? How can you use technology rather than letting it use you?`,
+        answer: ``
+    },
+    {
+        question: `Goodhart's Law states “When a measure becomes a target, it ceases to be a good measure.” What does this mean? When does tracking go too far? How can we know we're tracking the right measures with the right mindsets?`,
+        answer: ``
+    },
+    {
+        question: `Why is it important to “keep your identity small"? How do you balance this idea with the focus on identity-based habits discussed throughout the book?`,
+        answer: ``
+    }
+]
